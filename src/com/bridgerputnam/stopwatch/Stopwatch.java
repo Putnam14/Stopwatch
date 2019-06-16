99a8fc309fb2c085e1848d11c69ae6ab09249093
Putnam14 <putnambr76@gmail.com>
2019-06-15 21:59:58 -0600

    1,000,000 ns in a ms, not 1,000

--- src/com/bridgerputnam/stopwatch/Stopwatch.java
+++ src/com/bridgerputnam/stopwatch/Stopwatch.java
@@ -13,7 +13,7 @@ public class Stopwatch {
             start = System.nanoTime();
             r.run();
             end = System.nanoTime();
-            ms += (int) (end - start)/1000;
+            ms += (int) (end - start)/1000000;
         }
         if(samples > 1) {
             System.out.printf("Method took %dms on average, over %d calls.\n",ms/samples,samples);
